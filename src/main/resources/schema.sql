759ea220941fab98e2fd06b688084e35ca3bc93f
David Bašić <d.basic97.db@gmail.com>
2023-07-16 14:19:52 +0200

    commit

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,3 +9,18 @@ CREATE TABLE BOARDGAMES
     PRICE     DECIMAL(5, 2) NOT NULL,
     PRIMARY KEY (ID)
 );
+
+create table users
+(
+    username varchar_ignorecase(50)  not null primary key,
+    password varchar_ignorecase(500) not null,
+    enabled  boolean                 not null
+);
+
+create table authorities
+(
+    username  varchar_ignorecase(50) not null,
+    authority varchar_ignorecase(50) not null,
+    constraint fk_authorities_users foreign key (username) references users (username)
+);
+create unique index ix_auth_username on authorities (username, authority);
