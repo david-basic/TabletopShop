5da09a1857ec28db9cd65105cded001b7ba501d6
David Bašić <d.basic97.db@gmail.com>
2023-07-22 19:44:29 +0200

    web sec config

--- src/main/java/hr/algebra/tabletopshop/configuration/WebSecurityConfig.java
+++ src/main/java/hr/algebra/tabletopshop/configuration/WebSecurityConfig.java
@@ -2,33 +2,32 @@ package hr.algebra.tabletopshop.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
-                        .requestMatchers("/storeHome/homePage.html").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/storeHome/newBoardgame.html").hasAnyRole("ADMIN")
+                        .requestMatchers("/", "/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
-                        .loginPage("/login")
-                        //.successHandler(new MySimpleUrlAuthenticationSuccessHandler())
-                        .defaultSuccessUrl("/storeHome/homePage.html", true)
+                        .loginPage("/auth/login.html")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/storeHome/homePage.html", true) // true mi treba ovdje jer inace mi prikaze jquery ili bootstrap response
                         .permitAll()
                 )
                 .logout((logout) -> logout.permitAll());
@@ -37,42 +36,16 @@ public class WebSecurityConfig {
     }
     
     @Bean
-    public UserDetailsService userDetailsService(final AuthenticationManagerBuilder auth) throws Exception {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("user")
-//                .password(encoder().encode("password"))
-//                .roles("USER")
-//                .and()
-//                .withUser("admin")
-//                .password(encoder().encode("admin"))
-//                .roles("USER", "ADMIN");
-//
-//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public UserDetailsService jdbcUserDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authsByUsernameQuery = "select username, authority from authorities where username = ?";
         
-        final User.UserBuilder userBuilder = User.builder().passwordEncoder(this.encoder()::encode);
-        UserDetails user = userBuilder
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
         
-        UserDetails admin = userBuilder
-                .username("admin")
-                .password("password")
-                .roles("USER", "ADMIN")
-                .build();
+        users.setUsersByUsernameQuery(usersByUsernameQuery);
+        users.setAuthoritiesByUsernameQuery(authsByUsernameQuery);
         
-        return new InMemoryUserDetailsManager(user, admin);
+        return users;
     }
     
     @Bean
