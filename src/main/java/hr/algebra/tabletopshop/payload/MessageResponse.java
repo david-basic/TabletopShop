bdeb7f4eccf549896f0dfeb41e63c5c2352b96f4
David Bašić <d.basic97.db@gmail.com>
2023-08-15 17:11:47 +0200

    register now returns proper view and models

--- src/main/java/hr/algebra/tabletopshop/payload/MessageResponse.java
+++ src/main/java/hr/algebra/tabletopshop/payload/MessageResponse.java
@@ -2,9 +2,11 @@ package hr.algebra.tabletopshop.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MessageResponse {
     private String message;
 }
--- src/main/java/hr/algebra/tabletopshop/payload/UserLoginRequest.java
+++ src/main/java/hr/algebra/tabletopshop/payload/UserLoginRequest.java
@@ -1,9 +1,13 @@
 package hr.algebra.tabletopshop.payload;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserLoginRequest {
     @NotBlank
     private String username;
