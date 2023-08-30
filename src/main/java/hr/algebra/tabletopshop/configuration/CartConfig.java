6cd759ab3f85f07ec86fb61f7cc1b1dbf98fd644
David Bašić <d.basic97.db@gmail.com>
2023-08-30 21:12:05 +0200

    formatted

--- src/main/java/hr/algebra/tabletopshop/configuration/CartConfig.java
+++ src/main/java/hr/algebra/tabletopshop/configuration/CartConfig.java
@@ -1,13 +1,9 @@
 package hr.algebra.tabletopshop.configuration;
 
 import hr.algebra.tabletopshop.model.cart.Cart;
-import hr.algebra.tabletopshop.model.cart.CartItem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.annotation.SessionScope;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Configuration
 public class CartConfig {
