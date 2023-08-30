311283201b3ae4f3fa84367d77c5b2a108724919
David Bašić <d.basic97.db@gmail.com>
2023-08-30 21:06:53 +0200

    added cart and cart item

---
+++ src/main/java/hr/algebra/tabletopshop/model/cart/Cart.java
@@ -0,0 +1,85 @@
+package hr.algebra.tabletopshop.model.cart;
+
+import hr.algebra.tabletopshop.model.users.User;
+import hr.algebra.tabletopshop.repository.CartRepositoryMongo;
+import hr.algebra.tabletopshop.service.implementations.CartServiceImpl;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+@EqualsAndHashCode(doNotUseGetters = true, onlyExplicitlyIncluded = true)
+@Document("carts")
+public class Cart implements Serializable{
+    
+    @Id
+    private String id;
+    
+    @EqualsAndHashCode.Include
+    private Integer cartId;
+    
+    @EqualsAndHashCode.Include
+    @DBRef
+    private User user;
+    
+    private Double totalPrice;
+    
+    @DBRef
+    private Set<CartItem> cartItems;
+    
+    @Builder
+    public Cart(Integer cartId, User user, Double totalPrice, Set<CartItem> cartItems) {
+        this.cartId = cartId;
+        this.user = user;
+        this.totalPrice = Objects.requireNonNullElse(totalPrice, 0.0);
+        this.cartItems = Objects.requireNonNullElse(cartItems, new HashSet<>());
+    }
+    
+    public void addToTotal(Double value) {
+        this.totalPrice = this.totalPrice + value;
+    }
+    
+    public void subtractFromTotal(Double value) {
+        this.totalPrice = this.totalPrice - value;
+    }
+    
+    public void addItem(CartItem item) {
+        cartItems.add(item);
+        changeTotal();
+    }
+    
+    public void removeItem(CartItem item) {
+        cartItems.remove(item);
+        changeTotal();
+    }
+    
+    private void changeTotal() {
+        Double tempTotal = 0.0;
+        for (var item : cartItems) {
+            tempTotal += item.getItem().getPrice() * item.getQuantity();
+        }
+        totalPrice = tempTotal;
+    }
+    
+    public static Cart copyCart(Cart cart) {
+        return new Cart(
+                cart.getCartId(),
+                cart.getUser(),
+                cart.getTotalPrice(),
+                cart.getCartItems()
+        );
+    }
+}
---
+++ src/main/java/hr/algebra/tabletopshop/model/cart/CartItem.java
@@ -0,0 +1,64 @@
+package hr.algebra.tabletopshop.model.cart;
+
+import hr.algebra.tabletopshop.model.items.Item;
+import hr.algebra.tabletopshop.service.CartService;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(doNotUseGetters = true, onlyExplicitlyIncluded = true)
+@Document("cartitems")
+public class CartItem implements Serializable{
+    
+    @Id
+    private String id;
+    
+    @EqualsAndHashCode.Include
+    private Integer cartItemId;
+    
+    private Item item;
+    
+    private Integer quantity;
+    
+    @DBRef
+    private Cart cart;
+    
+    @Builder
+    public CartItem(Integer cartItemId, Item item, Integer quantity, Cart cart) {
+        this.cartItemId = cartItemId;
+        this.item = item;
+        this.quantity = quantity;
+        this.cart = cart;
+    }
+    
+    public Double getTotal() {
+        return this.item.getPrice() * this.quantity;
+    }
+    
+    public Double getItemPrice() {
+        return this.item.getPrice();
+    }
+    
+    public void incrementQuantity() {
+        this.quantity += 1;
+    }
+    
+    public void decrementQuantity() {
+        this.quantity -= 1;
+    }
+    
+    public void addQuantity(Integer quantity) {
+        this.quantity += quantity;
+    }
+    
+}
