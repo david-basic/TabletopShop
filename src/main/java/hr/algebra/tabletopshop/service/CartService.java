30ed71ffa1015e94e3981409c06c3e2e85ae5907
David Bašić <d.basic97.db@gmail.com>
2023-08-30 21:08:38 +0200

    created cart service

---
+++ src/main/java/hr/algebra/tabletopshop/service/CartService.java
@@ -0,0 +1,28 @@
+package hr.algebra.tabletopshop.service;
+
+import hr.algebra.tabletopshop.dto.ChangeItemQuantityDto;
+import hr.algebra.tabletopshop.dto.RemoveItemFromCartDto;
+import hr.algebra.tabletopshop.model.cart.Cart;
+import hr.algebra.tabletopshop.model.items.Item;
+import hr.algebra.tabletopshop.model.users.User;
+
+public interface CartService {
+    Cart getCurrentUserCart();
+    void saveCart(Cart cart);
+    
+    void addItemToCart(Item item, Integer quantity);
+    
+    void cleanCart();
+    
+    Cart getCartByUser(User user);
+    
+    Cart getCartById(String id);
+    
+    Cart getCartByItemId(Integer id);
+    
+    RemoveItemFromCartDto removeItemFromCart(Item item);
+    
+    ChangeItemQuantityDto incrementItemInCart(Item item);
+    
+    ChangeItemQuantityDto decrementItemInCart(Item item);
+}
