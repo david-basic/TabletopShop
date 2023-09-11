3cd3750d6936a76c7093acab8b62e3bd5299cf95
David Bašić <d.basic97.db@gmail.com>
2023-09-11 14:52:53 +0200

    project deployed, cart is a big buggy still

--- src/main/java/hr/algebra/tabletopshop/TabletopShopApplication.java
+++ src/main/java/hr/algebra/tabletopshop/TabletopShopApplication.java
@@ -2,12 +2,18 @@ package hr.algebra.tabletopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TabletopShopApplication {
+public class TabletopShopApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TabletopShopApplication.class, args);
     }
-
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TabletopShopApplication.class);
+    }
 }
